bbb6a947b764bc652c27b6f0d0c7c1ebe59a7c6d
Panupong Pasupat <ppasupat@stanford.edu>
2015-10-16 11:50:37 -0700

    Allow locking

--- server.py
+++ server.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, os, traceback
+import sys, os, traceback, datetime, platform
 basedir = os.path.dirname(os.path.abspath(__file__))
 
 PORT = 8080
 
 if __name__ == '__main__':
+    if os.path.exists('lock'):
+        with open('lock') as fin:
+            print >> sys.stderr, fin.read()
+        print >> sys.stderr, ('Lock file exists. ' +
+            'Please close all other instances to prevent corrupted files.')
+        exit(1)
     from a9.app import start
+    with open('lock', 'w') as fout:
+        print >> fout, 'Locked at %s on %s' %\
+            (datetime.datetime.now().isoformat(), platform.node())
     try:
         start(PORT, basedir)
     except:
         print traceback.format_exc()
+    os.remove('lock')
